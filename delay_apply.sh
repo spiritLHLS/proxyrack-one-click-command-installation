764c48720e7c1e4607ea2b7b61195453fcf8aad9
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-04-14 11:49:12 +0000

    update

--- delay_apply.sh
+++ delay_apply.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # FROM https://github.com/spiritLHLS/proxyrack-one-click-command-installation
-# 2024.01.22
+# 2024.04.14
 
 PRTOKEN="$1"
 uuid=$(cat /usr/local/bin/proxyrack_uuid)
@@ -10,6 +10,17 @@ dname=$(cat /usr/local/bin/proxyrack_dname)
 sleep 5m
 
 # 第一次请求
+curl -s \
+  -X POST https://peer.proxyrack.com/api/device/add \
+  -H "Api-Key: $PRTOKEN" \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -d "{\"device_id\":\"$uuid\",\"device_name\":\"$dname\"}"
+
+# 等待5分钟
+sleep 5m
+
+# 第二次请求
 curl -s \
   -X POST https://peer.proxyrack.com/api/device/add \
   -H "Api-Key: $PRTOKEN" \
